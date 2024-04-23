70229693ec3f37d879d96c7146b3a543b6212b11
GabyShamblin <gabyshamblin@gmail.com>
2024-04-22 22:15:27 -0400

    Added ship and some logic

--- Assets/Scripts/EarthSpin.cs
+++ Assets/Scripts/EarthSpin.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EarthSpin : MonoBehaviour
 {
     [Tooltip("Earth rotates in the negative direction.")]
-    [SerializeField] private int spinSpeed = -10;
+    [SerializeField] private float spinSpeed = -1f;
 
     void Update() {
         transform.Rotate(0f, spinSpeed, 0f, Space.Self);
--- Assets/Scripts/ScriptedEvents.cs
+++ Assets/Scripts/ScriptedEvents.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ScriptedEvents : MonoBehaviour
 {
+    [Header("Skybox Transition")]
     public Material skybox;
+    public ParticleSystem zoom;
     public bool blendStart = false;
     public float blendTime = 5f;
     public float blend = 0f;
+    private float zoomStart = 0.2f;
+    private float zoomEnd = 1.5f;
     private float time = 0f;
+
+    [Header("Open Doors")]
+    public Animator doors;
+    public bool doorStart = false;
 
     void Start()
     {
         skybox.SetFloat("_Blend", 0);
+        // zoom.Stop();
     }
 
     void Update()
@@ -21,11 +30,34 @@ public class ScriptedEvents : MonoBehaviour
             blend += Time.deltaTime / blendTime;
             skybox.SetFloat("_Blend", blend);
 
+            if (zoom.isPlaying && blend >= zoomEnd) {
+                Debug.Log("Stop zoom");
+                zoom.Stop();
+            }
+            else if (!zoom.isPlaying && blend >= zoomStart) {
+                Debug.Log("Play zoom");
+                zoom.Play();
+            }
+
             if (blend >= 2) {
                 blendStart = false;
                 blend = 0f;
+                time = 0f;
             }
         }
 
+        if (doorStart) {
+            openDoors();
+        }
+    }
+
+    public void takeOff() {
+        blendStart = true;
+    }
+
+    public void openDoors() {
+        if (doors != null) {
+            doors.Play("OpenDoors");
+        }
     }
 }
